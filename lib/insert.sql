07c8ab13a2496cb6a5c31092584065cc81a14028
Jacob Ives <jacob.w.ives@gmail.com>
2023-06-14 16:31:43 -0600

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Feisty', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'F', 'Black', 'Rowdy', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 9, 'F', 'White', 'Calm', 1);
+INSERT INTO bears VALUES (4, 'Seargent Brown', 5, 'F', 'Brown', 'Strict', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 6, 'F', 'Black', 'Calm', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 45, 'M', 'Green', 'Mean', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 8, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears VALUES (8, NULL, 856, 'M', 'complicated', 'omnipotent', 1);
